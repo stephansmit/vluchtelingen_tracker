19d91b51e267388fad33f8e5c0ecb8faa56dec3c
anonymous <s.h.h.j.smit@tudelft.nl>
2019-09-19 20:55:25 +0200

    cleaned up

--- track.py
+++ track.py
@@ -8,12 +8,9 @@ url = 'https://www.rijksoverheid.nl/documenten/publicaties/2019/01/14/de-asielin
 html_doc = urllib.request.urlopen(url)
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-mydivs = soup.findAll("a", {"class": "news"})
-numbers = []
-weeks = []
-for i in mydivs:
-    weeks.append(int(i.findAll('h3')[0].text.strip().split(' ')[4]))
-    numbers.append(float(i.findAll('h3')[0].text.strip().split(' ')[-1]))
+divs = soup.findAll("a", {"class": "news"})
+weeks = [int(i.findAll('h3')[0].text.strip().split(' ')[4]) for i in divs]
+numbers = [float(i.findAll('h3')[0].text.strip().split(' ')[-1]) for i in divs]
 
 df = pd.DataFrame(np.array([weeks, numbers]).T, columns=['week','aantal']).sort_values('week')
 df['week']=df['week'].apply(lambda x: int(x))
@@ -23,7 +20,7 @@ df['cumulatief']= df['aantal'].cumsum()
 
 fig, ax = plt.subplots(figsize=(20,10))
 ax2 = ax.twinx()
-df.plot(x='week',y='aantal',ax=ax, kind='bar', legend=None)
+df.plot(x='week',y='aantal',ax=ax, kind='bar', legend=None, color='b')
 df.plot(x='week2',y='cumulatief',ax=ax2, color='r', linewidth=4, legend=None)
 ax2.grid()
 ax.set_xlabel("Week van het jaar", fontsize=24, labelpad=20)
@@ -34,4 +31,5 @@ ax2.tick_params(labelsize=20, color='b',axis='y', labelcolor='r')
 ax.tick_params(labelsize=20, color='k',axis='x', labelcolor='k')
 ax.set_xlim([-1,37])
 ax.set_title("Vluchtelingen 2019", fontsize=30, pad=20)
+fig.tight_layout()
 fig.savefig("vluchtelingen.png")
